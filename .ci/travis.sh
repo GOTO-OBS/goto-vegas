65d7c0cfcc20ad5f4451ab03e9fd538c7a006587
Andy Casey <andycasey@gmail.com>
2017-07-27 21:46:41 +1000

    update install script

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -1,17 +1,19 @@
 #!/bin/bash -x
 
-# Download Conda
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+
+# Download and install miniconda
+sudo apt-get update
+if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+  wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
+else
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+fi
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
-
-# Install Conda
-conda config --set always_yes yes
+hash -r
+conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda info -a
-conda create --yes -n py python=$PYTHON_VERSION
-source activate py
-conda install numpy scipy scikit-learn
 
 # Download training set
 python scripts/download_training_set.py
