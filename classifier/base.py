2678684d4db0f44bdf84fa383ee990bcf737836e
Andy Casey <andycasey@gmail.com>
2017-07-27 21:35:01 +1000

    init travis

---
+++ classifier/base.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function
+import numpy as np
+
+__all__ = ["BaseClassifier"]
+
+class BaseClassifier(object):
+
+    def __init__(self):
+        return None
---
+++ classifier/classifier.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function
+from .base import BaseClassifier
+
+class Classifier(BaseClassifier):
+    pass
