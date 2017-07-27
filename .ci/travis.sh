186bc934816f8f5f43bfd52e90a35a13488b33f9
Andy Casey <andycasey@gmail.com>
2017-07-27 21:27:33 +1000

    Initial commit

---
+++ .ci/travis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+# Download Conda
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+
+# Install Conda
+conda config --set always_yes yes
+conda update -q conda
+conda info -a
+conda create --yes -n py python=$PYTHON_VERSION
+source activate py
+conda install numpy scipy scikit-learn
---
+++ README.md
@@ -0,0 +1,12 @@
+GOTO Vegas
+----------
+Classify transient sources, and throw away the junk.
+
+Authors
+-------
+- Andrew R. Casey (Monash)
+- Duncan Galloway (Monash)
+- Evert Rol (Monash)
+
+Leaderboard
+-----------
