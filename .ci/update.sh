b848c9a48fc1d69619289c1f4a8791eb1911fa6d
Andy Casey <andycasey@gmail.com>
2017-07-28 19:49:31 +1000

    fix travis?

--- .ci/update.sh
+++ .ci/update.sh
@@ -3,6 +3,7 @@
 # Push the updated entries.csv and README.md file to GitHub
 if [ -n "$GITHUB_API_KEY" ]; then
   cd $TRAVIS_BUILD_DIR
+  git checkout -b master
   git add entries.csv README.md
   git -c user.name="referee" -c user.email="referee" commit -m "Updating scoreboard [ci skip]"
   git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/goto-obs/goto-vegas master
