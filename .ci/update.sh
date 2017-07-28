2fe59045e95ea0dcd40431b8c1533c71f8bc7a56
Andy Casey <andycasey@gmail.com>
2017-07-28 20:14:36 +1000

    fuck

--- .ci/update.sh
+++ .ci/update.sh
@@ -3,7 +3,8 @@
 # Push the updated entries.csv and README.md file to GitHub
 if [ -n "$GITHUB_API_KEY" ]; then
   cd $TRAVIS_BUILD_DIR
-  git checkout -b master
+  #git checkout -b master
+  git reset --hard $TRAVIS_COMMIT
   git add entries.csv README.md
   git -c user.name="referee" -c user.email="referee" commit -m "Updating scoreboard [ci skip]"
   git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/goto-obs/goto-vegas master
