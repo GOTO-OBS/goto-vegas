5f91179ecd1fd825be71dc205a1881d0c45e21d8
Andy Casey <andycasey@gmail.com>
2017-07-30 17:37:29 +1000

    Add baseline

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import division, print_function
+import numpy as np
 from .base import BaseClassifier
 
 class Classifier(BaseClassifier):
@@ -33,4 +34,52 @@ class Classifier(BaseClassifier):
         :returns:
             A single-valued classification for this object.
         """
-        return 0
+
+        weights = {
+            "fpos3": 2,
+            "nsat5": 2,
+            "nsat7": 1,
+            "fneg3": 1,
+            "n3sig7": 2,
+            "src-s2n": 0.5,
+            "hasid": 2
+        }
+
+        score = {}
+        score["hasid"] = (predictors["photid"] > 0).astype(int)
+        if predictors["nsat5"] > 5:
+            score["nsat5"] = -0.5
+        elif predictors["nsat5"] > 2:
+            score["nsat5"] = -0.2
+
+        if predictors["nsat7"] > 15:
+            score["nsat7"] = -1
+
+        if predictors["fneg3"] < 0.01:
+            score["fneg3"] = 0.5
+
+        elif predictors["fneg3"] < 0.1:
+            score["fneg3"] = 0.2
+
+        if predictors["src-s2n"] > 50:
+            score["src-s2n"] = 0.5
+
+        if predictors["n2sig5"] <= 2:
+            score["n2sig5"] = 0.5
+
+        if predictors["n2sig7"] <= 2:
+            score["n2sig7"] = 0.5
+
+        if predictors["n3sig5"] <= 2:
+            score["n3sig5"] = 0.5
+
+        score["n3sig7"] = np.exp(-predictors["n3sig7"]/10.0) - 1.0
+
+        weighted_score = 0
+        for key, weight in weights.items():
+            weighted_score += score.get(key, 0) * weight
+
+        weighted_score /= sum(weights.values())
+
+        # Added some cut.
+        return (weighted_score > 0.23).astype(int)
