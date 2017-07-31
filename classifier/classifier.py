3e3e7235a58ab6555825f2c79dbe5a51459f42aa
Evert Rol <evert.rol@gmail.com>
2017-07-31 14:04:12 +1000

    Simple random forest classifier
    Uses the basic scikit-learn random forest classifier. No specific
    tweaks to its parameters are made, other than that the `class_weight`
    parameter is set to 'balanced'.

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -2,6 +2,9 @@
 
 from __future__ import division, print_function
 from .base import BaseClassifier
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
 
 import numpy as np
 
@@ -21,6 +24,15 @@ class Classifier(BaseClassifier):
             This array should have the same length as the number of predictor
             rows.
         """
+
+        df = predictors.to_pandas()  #  RFC doesn't like Astropy tables
+        df = df.fillna(0)  # RCS doesn't like NaNs.
+                           # Potentially, a different (large, negative?)
+                           # value may be better. To test and find out
+        self.clf = RandomForestClassifier(n_estimators=100, max_features='sqrt',
+                                          n_jobs=4, class_weight='balanced')
+        self.clf.fit(df, classifications)
+
         return None
 
 
@@ -29,9 +41,11 @@ class Classifier(BaseClassifier):
         Classify a single object, given some predictors.
 
         :param predictors:
-            A row of predictors for a single object.
+            A row of predictors for a single object.
 
         :returns:
             A single-valued classification for this object.
         """
-        return np.zeros(len(predictors))
+
+        data = np.array(predictors.as_void().tolist()).reshape(1, -1)
+        return self.clf.predict(data)
