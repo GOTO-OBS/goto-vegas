165655c474774359c34de908ae4e700399e771d3
Andy Casey <andycasey@gmail.com>
2017-07-31 14:06:06 +1000

    trigger a travis build to run on the full simulated data set

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -6,19 +6,18 @@ from .base import BaseClassifier
 
 class Classifier(BaseClassifier):
 
-
     def train(self, predictors, classifications, **kwargs):
         """
         Train the model based on a table of predictors and known classifications
         for objects in a training set.
-         
+
         :param predictors:
             An :class:`astropy.table.Table` of possible predictors, where the
             number of rows is the number of objects in the training set.
-           
+
         :param classifications:
             An array of classifications for all objects in the training set.
-            This array should have the same length as the number of predictor 
+            This array should have the same length as the number of predictor
             rows.
         """
         return None
@@ -27,10 +26,10 @@ class Classifier(BaseClassifier):
     def classify(self, predictors, **kwargs):
         """
         Classify a single object, given some predictors.
-        
+
         :param predictors:
             A row of predictors for a single object.
-           
+
         :returns:
             A single-valued classification for this object.
         """
