0dd6c5a6bdfdbb94046adb9e36e0d38f81895725
Evert Rol <evert.rol@gmail.com>
2017-07-31 13:48:25 +1000

    Remove trailing whitespace

--- classifier/base.py
+++ classifier/base.py
@@ -24,15 +24,15 @@ class BaseClassifier(object):
         :param predictors:
             An :class:`astropy.table.Table` of possible predictors, where the
             number of rows is the number of objects in the validation set.
-           
+
         :param classifications:
             An array of classifications for all objects in the validation set.
-            This array should have the same length as the number of predictor 
+            This array should have the same length as the number of predictor
             rows.
 
         :returns:
             A four-length tuple containing the number of true transients found,
-            the number of transients missed, the number of false positives, 
+            the number of transients missed, the number of false positives,
             and the calculated score.
         """
 
@@ -54,7 +54,7 @@ class BaseClassifier(object):
         score = fbeta_score(classifications, model_classifications, beta=2)
 
         return (
-            N_transients_found, 
+            N_transients_found,
             N_transients_missed,
             N_false_positives,
             score
